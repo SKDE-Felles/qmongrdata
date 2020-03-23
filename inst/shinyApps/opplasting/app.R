9d9cfabe1a80906b71fc0af3c262b84768657b93
arnfinn <arnfinn.steindal@gmail.com>
2020-03-23 20:47:21 +0100

    fixed most of lintr warnings

--- inst/shinyApps/opplasting/app.R
+++ inst/shinyApps/opplasting/app.R
@@ -5,16 +5,16 @@ KvalIndData$Aar <- as.integer(KvalIndData$Aar)
 # Define UI for data upload app ----
 ui <- fluidPage(
   shinyjs::useShinyjs(),
-  
+
   # App title ----
   # titlePanel("Last opp filer"),
-  
+
   # Sidebar layout with input and output definitions ----
   sidebarLayout(
-    
+
     # Sidebar panel for inputs ----
     sidebarPanel(
-      
+
       # Input: Select a file ----
       fileInput("file1", "Velg csv-fil",
                 multiple = FALSE,
@@ -22,10 +22,10 @@ ui <- fluidPage(
                            "text/comma-separated-values,text/plain",
                            ".csv"),
                 ),
-      
+
       # Horizontal line ----
       tags$hr(),
-      
+
       # # Input: Checkbox if file has header ----
       # checkboxInput("header", "Header", TRUE),
 
@@ -35,56 +35,58 @@ ui <- fluidPage(
                                Komma = ",",
                                Tab = "\t"),
                    selected = ";"),
-      
+
       radioButtons("dec_sep", "Desimalseparator",
                    choices = c(Puktum = ".",
                                Komma = ","),
                    selected = ","),
-      
+
       radioButtons("tegnsett", "Tegnsetting",
                    choices = c("LATIN1", "UTF-8", "Annet"), #, "UTF-8-BOM"
                    selected = "UTF-8"),
-      
-      uiOutput(outputId = 'andretegn'),
-      
-      
+
+      uiOutput(outputId = "andretegn"),
+
       # Input: Select number of rows to display ----
       radioButtons("disp", "Vis",
-                   choices = c('Første 20 rader' = "head",
-                               '20 tilfeldige rader' = 'tilf20',
-                               'Alle rader' = "all"),
+                   choices = c("Første 20 rader" = "head",
+                               "20 tilfeldige rader" = "tilf20",
+                               "Alle rader" = "all"),
                    selected = "head"),
-      
+
       tags$hr(),
-      
-      shinyjs::disabled(actionButton(inputId = "lastopp", label = "Send til server", icon("paper-plane"), 
-                   style="color: #fff; background-color: #337ab7; border-color: #2e6da4")),
-      shinyjs::hidden(p(id = "text1", "Fiks følgende problemer med datasettet for å aktivisere 
+
+      shinyjs::disabled(actionButton(inputId = "lastopp", label = "Send til server", icon("paper-plane"),
+                   style = "color: #fff; background-color: #337ab7; border-color: #2e6da4")),
+      shinyjs::hidden(p(id = "text1", "Fiks følgende problemer med datasettet for å aktivisere
                         opplastingsknapp: ")),
       uiOutput("feilmeld")
-      # textInput("tittel", "Skriv inn registernavn", "")
-      # checkboxInput('tmptull', 'Aktiver knapp')
-      
     ),
-    
+
     # Main panel for displaying outputs ----
     mainPanel(
       titlePanel("Last opp fil"),
-      h4("Dette er et verktøy for å hjelpe med å laste opp filer i riktig format til bruk i sykehusviser. Under 
-         er det vist et eksempel på hvordan filen skal se ut. Din fil skal være i csv-format med de samme 
-         kolonnenavnene som i eksempelet. Det er ikke nøye med små og store bokstaver eller rekkefølgen på 
+      h4("Dette er et verktøy for å hjelpe med å laste opp filer i riktig format til bruk i sykehusviser. Under
+         er det vist et eksempel på hvordan filen skal se ut. Din fil skal være i csv-format med de samme
+         kolonnenavnene som i eksempelet. Det er ikke nøye med små og store bokstaver eller rekkefølgen på
          kolonnene. Under følger en forklaring på innholdet i kolonnene."),
-      
-      div(class = "container", style ="margin-right:(@gutter / 10)" ,
+
+      div(class = "container", style = "margin-right:(@gutter / 10)",
           tags$ul(
-            tags$li(h5(tags$b(names(KvalIndData)[1]), ": året for hendelsen")),
-            tags$li(h5(tags$b(names(KvalIndData)[2]), ": registerets navn på enheten knyttet til hendelsen")),
-            tags$li(h5(tags$b(names(KvalIndData)[3]), ": registerets numeriske ID (ofte RESH) på enheten knyttet til hendelsen")),
-            tags$li(h5(tags$b(names(KvalIndData)[4]), ": organisasjonsnummeret til sykehus/HF knyttet til hendelsen,
+            tags$li(h5(tags$b(names(KvalIndData)[1]),
+                       ": året for hendelsen")),
+            tags$li(h5(tags$b(names(KvalIndData)[2]),
+                       ": registerets navn på enheten knyttet til hendelsen")),
+            tags$li(h5(tags$b(names(KvalIndData)[3]),
+                       ": registerets numeriske ID (ofte RESH) på enheten knyttet til hendelsen")),
+            tags$li(h5(tags$b(names(KvalIndData)[4]),
+                       ": organisasjonsnummeret til sykehus/HF knyttet til hendelsen,
                        hentet fra tilsendt fil SykehusNavnStruktur")),
-            tags$li(h5(tags$b(names(KvalIndData)[5]), ": enten en binær indikator (0/1) for fremstilling av andel måloppnåelse,
+            tags$li(h5(tags$b(names(KvalIndData)[5]),
+                       ": enten en binær indikator (0/1) for fremstilling av andel måloppnåelse,
                        eller en numerisk variabel for fremstilling av gjennomsnitt eller median")),
-            tags$li(h5(tags$b(names(KvalIndData)[6]), ": identifikator for kvalitetsindikator, skal samsvare med 
+            tags$li(h5(tags$b(names(KvalIndData)[6]),
+                       ": identifikator for kvalitetsindikator, skal samsvare med
                        identifikator gitt i Indikatorbeskrivelse"))
           )
       ),
@@ -95,17 +97,16 @@ ui <- fluidPage(
       # Horizontal line ----
       tags$hr(),
       h4("Opplastet datasett:"),
-      h5("Her vises en utsnitt av datasett du har lastet opp. Juster parametrene i menyen på venstresiden 
+      h5("Her vises en utsnitt av datasett du har lastet opp. Juster parametrene i menyen på venstresiden
          slik at norske tegn (æøå) representeres riktig og tallene fremstår slik de skal."),
       tableOutput("contents")
     )
-    
   )
 )
 
 # Define server logic to read selected file ----
 server <- function(input, output) {
-  
+
   observe(
       if (datatester()$test2 & datatester()$test3 & datatester()$test4) {
         shinyjs::enable("lastopp")
@@ -115,31 +116,32 @@ server <- function(input, output) {
         shinyjs::show("text1")
       }
   )
-  
-  
+
   output$andretegn <- renderUI({
-    if (input$tegnsett == 'Annet') {
-      selectInput(inputId = 'andretegn_verdi', label = "Spesifiser:",
-                  choices = iconvlist(), selected = 'MS-ANSI')}
+    if (input$tegnsett == "Annet") {
+      selectInput(inputId = "andretegn_verdi", label = "Spesifiser:",
+                  choices = iconvlist(), selected = "MS-ANSI")
+    }
   })
-  
+
   df <- reactive({
     req(input$file1)
-    if (input$tegnsett == 'Annet') {
+    if (input$tegnsett == "Annet") {
       if (!is.null(input$andretegn_verdi)) {
-        tegn <- input$andretegn_verdi} 
-      else {tegn <- 'MS-ANSI'}
-          }
-    else {tegn <- input$tegnsett}
+        tegn <- input$andretegn_verdi
+      } else {
+        tegn <- "MS-ANSI"
+      }
+    } else {
+      tegn <- input$tegnsett
+    }
     df <- read.csv(input$file1$datapath,
                    header = TRUE,
                    sep = input$sep,
                    fileEncoding = tegn,
                    stringsAsFactors = FALSE)
   })
-  
-  
-  
+
   datatester <- function() {
     req(df())
     # Test 1: Riktig antall kolonner i opplasting (fjernet, dekkes av test 2 og 3)
@@ -147,93 +149,83 @@ server <- function(input, output) {
     # Test 3: Alle kolonnenavn i KvalIndData finnes i opplasting
     # Test 3: Alle kolonnenavn i KvalIndData finnes i opplasting
     # Test 4: Alle OrgNrShus i opplasting finnes i SykehusNavnStruktur
-    
+
     test1 <- length(names(df())) == length(names(KvalIndData))
-    test1tekst <- paste0('Opplastet fil har ', dim(df())[2], ' kolonner. Den skal ha 6 kolonner.')
+    test1tekst <- paste0("Opplastet fil har ", dim(df())[2], " kolonner. Den skal ha 6 kolonner.")
     test2 <- length(setdiff(tolower(names(df())), tolower(names(KvalIndData)))) == 0
-    test2tekst <- paste0('Følgende kolonnenavn i opplastet fil er ikke å finne som skrevet i 
-                         eksempeldata: ', 
-                         paste0(names(df())[match(setdiff(tolower(names(df())), 
-                                                          tolower(names(KvalIndData))), tolower(names(df())))], 
-                                collapse = ', '))
+    test2tekst <- paste0("Følgende kolonnenavn i opplastet fil er ikke å finne som skrevet i
+                         eksempeldata: ",
+                         paste0(names(df())[match(setdiff(tolower(names(df())),
+                                                          tolower(names(KvalIndData))), tolower(names(df())))],
+                                collapse = ", "))
     test3 <- length(setdiff(tolower(names(KvalIndData)), tolower(names(df())))) == 0
-    test3tekst <- paste0('Følgende kolonner mangler eller er feilskrevet i opplastet fil: ', 
-                         paste0(names(KvalIndData)[match(setdiff(tolower(names(KvalIndData)), 
-                                                                 tolower(names(df()))), tolower(names(KvalIndData)))], 
-                                collapse = ', '))
-    test4 <- length(setdiff(df()[, match(tolower("OrgNrShus"), tolower(names(df())))], 
-                            c(SykehusNavnStruktur$OrgNrRHF, SykehusNavnStruktur$OrgNrHF, 
+    test3tekst <- paste0("Følgende kolonner mangler eller er feilskrevet i opplastet fil: ",
+                         paste0(names(KvalIndData)[match(setdiff(tolower(names(KvalIndData)),
+                                                                 tolower(names(df()))), tolower(names(KvalIndData)))],
+                                collapse = ", "))
+    test4 <- length(setdiff(df()[, match(tolower("OrgNrShus"), tolower(names(df())))],
+                            c(SykehusNavnStruktur$OrgNrRHF, SykehusNavnStruktur$OrgNrHF,
                                                     SykehusNavnStruktur$OrgNrShus))) == 0
-    test4tekst <- paste0('Følgende organisasjonsnummer i opplastet fil finnes ikke i tilsendt 
-                         mappingfil: ', 
-                         paste0(setdiff(df()[, match(tolower("OrgNrShus"), tolower(names(df())))], 
-                                        c(SykehusNavnStruktur$OrgNrRHF, SykehusNavnStruktur$OrgNrHF, 
-                                          SykehusNavnStruktur$OrgNrShus)), 
-                                collapse = ', '))
-    list(test1=test1, test2=test2, test3=test3, test4=test4,
-         test1tekst=test1tekst, test2tekst=test2tekst, test3tekst=test3tekst, test4tekst=test4tekst)
+    test4tekst <- paste0("Følgende organisasjonsnummer i opplastet fil finnes ikke i tilsendt
+                         mappingfil: ",
+                         paste0(setdiff(df()[, match(tolower("OrgNrShus"), tolower(names(df())))],
+                                        c(SykehusNavnStruktur$OrgNrRHF, SykehusNavnStruktur$OrgNrHF,
+                                          SykehusNavnStruktur$OrgNrShus)),
+                                collapse = ", "))
+    list(test1 = test1, test2 = test2, test3 = test3, test4 = test4,
+         test1tekst = test1tekst, test2tekst = test2tekst, test3tekst = test3tekst, test4tekst = test4tekst)
   }
-  
-  output$feilmeld <- renderUI({
+
+  output$feilmeld <- shiny::renderUI({
     data_test <- datatester()
-    tagList(
-        tags$ul(
-          # if (!data_test$test1) {tags$li(h5(tags$b(data_test$test1tekst)))},
-          if (!data_test$test2) {tags$li(h5(tags$b(data_test$test2tekst)))},
-          if (!data_test$test3) {tags$li(h5(tags$b(data_test$test3tekst)))},
-          if (!data_test$test4) {tags$li(h5(tags$b(data_test$test4tekst)))}
+    shiny::tagList(
+      shiny::tags$ul(
+          if (!data_test$test2) {
+            tags[["li"]](h5(tags[["b"]](data_test[["test2tekst"]])))
+            },
+          if (!data_test$test3) {
+            tags[["li"]](h5(tags[["b"]](data_test[["test3tekst"]])))
+            },
+          if (!data_test$test4) {
+            tags[["li"]](h5(tags[["b"]](data_test[["test4tekst"]])))
+            }
         )
     )
   })
-  
-  
+
   output$contents <- renderTable({
-    
-    # req(df())
-    
-    if(input$disp == "head") {
+
+    if (input$disp == "head") {
       df()[1:20, ]
     } else {
-      if(input$disp == "tilf20") {
+      if (input$disp == "tilf20") {
         df()[sample(1:dim(df())[1], 20), ]
       } else {
         return(df())
       }
     }
-    
   })
-  
+
   output$eksempel <- renderTable({
     KvalIndData[sample(1:dim(KvalIndData)[1], 6), ]
   })
-  
+
   # Last opp data til tempdir med filnavn registernavn og dato
   observeEvent(input$lastopp, {
     opplast <- df()[, match(tolower(names(df())), tolower(names(KvalIndData)))]
     names(opplast) <- names(KvalIndData)
-    assign(paste0(paste0(unique( stringr::str_extract(opplast$KvalIndID, "[aA-zZ]+") ), collapse = '_'),
-                  format(Sys.Date(), '%Y_%m_%d')), opplast)
-    # write.csv2(opplast, normalizePath(paste0(tempdir(), '\\',
-    #                                          paste0(unique( stringr::str_extract_all(df()$KvalIndID, "[aA-zZ]+") ), collapse = '_'),
-    #                                          Sys.Date(), '.csv')),
-    #            row.names = F, fileEncoding = 'UTF-8')
-    # save(opplast, file = normalizePath(paste0('data\\', paste0(unique( stringr::str_extract_all(df()$KvalIndID, "[aA-zZ]+") ),
-    #                                                            collapse = '_'), Sys.Date(), '.rda')))
-    # usethis::use_data(as.name(paste0(paste0(unique( stringr::str_extract_all(opplast$KvalIndID, "[aA-zZ]+") ), collapse = '_'), 
-    #                                    format(Sys.Date(), '%Y_%m_%d'))))
-    # usethis::use_data(get(paste0(paste0(unique( stringr::str_extract_all(opplast$KvalIndID, "[aA-zZ]+") ), collapse = '_'), 
-                                 # format(Sys.Date(), '%Y_%m_%d'))), overwrite = TRUE)
+    assign(paste0(paste0(unique(stringr::str_extract(opplast$KvalIndID, "[aA-zZ]+")), collapse = "_"),
+                  format(Sys.Date(), "%Y_%m_%d")), opplast)
     path <- usethis::proj_get()
     dir_data <- fs::path(path, "data-raw")
-    paths <- fs::path(dir_data, paste0(paste0(unique( stringr::str_extract(opplast$KvalIndID, "[aA-zZ]+") ), collapse = '_'),
-                                   format(Sys.Date(), '%Y_%m_%d')), ext = "rda")
-    save(list = paste0(paste0(unique( stringr::str_extract(opplast$KvalIndID, "[aA-zZ]+") ), collapse = '_'),
-                format(Sys.Date(), '%Y_%m_%d')), file = paths)
-    showNotification("Fil lagret på server, fred være med deg mitt barn.", type = "message")
+    paths <- fs::path(dir_data, paste0(paste0(unique(stringr::str_extract(opplast$KvalIndID,
+                                                                          "[aA-zZ]+")),
+                                              collapse = "_"),
+                                   format(Sys.Date(), "%Y_%m_%d")), ext = "rda")
+    save(list = paste0(paste0(unique(stringr::str_extract(opplast$KvalIndID, "[aA-zZ]+")), collapse = "_"),
+                format(Sys.Date(), "%Y_%m_%d")), file = paths)
+    shiny::showNotification("Fil lagret på server, fred være med deg mitt barn.", type = "message")
   })
-  
-  
-  
 }
 # Run the app ----
 shinyApp(ui, server)
