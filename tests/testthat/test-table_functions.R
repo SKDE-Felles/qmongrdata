e8ab4215248a36d91e9c883df6fbb210e9f992f8
Arnfinn Hykkerud Steindal <arnfinn.steindal@gmail.com>
2020-07-30 16:23:01 +0200

    Test update: use expect_equal_to_reference (#26)

--- tests/testthat/test-table_functions.R
+++ tests/testthat/test-table_functions.R
@@ -28,19 +28,17 @@ test_that("load_data", {
     "hospital_name_structure"
   )
 
-  qm_data <- readRDS("data/load_data1.rds")
-
-  expect_equal(
-    names(load_data("description")[["description"]]), names(qm_data$description)
+  expect_equal_to_reference(
+    names(load_data("description")[["description"]]), "data/description.rds"
   )
 
-  expect_equal(
+  expect_equal_to_reference(
     names(load_data("hospital_name_structure")[["hospital_name_structure"]]),
-    names(qm_data$hospital_name_structure)
+    "data/hospital_name_structure.rds"
   )
 
-  expect_equal(
+  expect_equal_to_reference(
     names(load_data("indicator")[["indicator"]]),
-    names(qm_data$indicator)
+    "data/indicator.rds"
   )
 })
