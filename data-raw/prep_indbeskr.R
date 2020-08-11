d7ec75ad8a9075a4bc62d0a4c1b71b7c7f107440
arnfinn <arnfinn.steindal@gmail.com>
2020-08-11 13:49:01 +0200

    dg_id = IndID for indikatorType == "dg"

--- data-raw/prep_indbeskr.R
+++ data-raw/prep_indbeskr.R
@@ -8,6 +8,7 @@ usethis::use_data(IndBeskr, overwrite = TRUE)
 ###### Legg på dekningsgrad ##########
 IndBeskr <- read.csv2("data-raw/IndBeskr.csv", fileEncoding = "UTF-8")
 
+
 IndBeskr <- IndBeskr %>%
   dplyr::mutate(dg_id = dplyr::case_when(Register == "hoftebrudd" & indikatorType != "dg" ~ "hoftebrudd_dekgrad_prim_brudd",
                                          Register == "lymfoid" & indikatorType != "dg" ~ "lymfoid_dekgrad_utred",
@@ -20,9 +21,11 @@ IndBeskr <- IndBeskr %>%
                                                                              "norkar_anb_med_forsnev_hals") & indikatorType != "dg" ~ "norkar_dg_forsnev_hals",
                                          Register == "norkar" & IndID == "norkar_anb_med_forsnev_ben" & indikatorType != "dg" ~ "norkar_dg_forsnev_ben",
                                          Register == "nger" & indikatorType != "dg" ~ "nger_dekgrad",
-                                         Register == "hjerneslag" & indikatorType != "dg" ~ "hjerneslag_dekgrad"))
+                                         Register == "hjerneslag" & indikatorType != "dg" ~ "hjerneslag_dekgrad",
+                                         indikatorType == "dg" ~ IndID))
 
-write.csv2(IndBeskr, file = "data-raw/IndBeskr.csv", fileEncoding = "UTF-8", )
+
+write.csv2(IndBeskr, file = "data-raw/IndBeskr.csv", fileEncoding = "UTF-8", row.names = FALSE)
 
 usethis::use_data(IndBeskr, overwrite = TRUE)
 
